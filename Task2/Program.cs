a093172ef8f02e80d14f2b31148c2e0e892e3e4d
Alexey <pwz-and_10@mail.ru>
2022-08-21 19:03:24 +0600

    create task2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write($"Введите элементы массива: ");
+        int numElem = Convert.ToInt32(Console.ReadLine());
+        int RandomNumbers(int numElem, int min, int max)
+        {
+            int[] randomNumb = new int[numElem];
+            int summElem = 0;
+            Console.Write("Результат: ");
+            for (int i = 0; i < randomNumb.Length; i++)
+            {
+                randomNumb[i] = new Random().Next(min, max);
+                Console.Write(randomNumb[i] + " ");
+                if (i % 2 != 1)
+                {
+                    summElem = summElem + randomNumb[i];
+                }
+            }
+            return summElem;
+        }
+
+        int randomNumb = RandomNumbers(numElem, 1, 10);
+        Console.WriteLine($"Сумма: {randomNumb}");
+    }
+}
